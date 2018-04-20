5c9e3b0d8681b167302e4191d46be921a8a2e20e
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-04-20 12:05:21 +0800

    *modify versioning info

--- version.py
+++ version.py
@@ -15,12 +15,12 @@ VSVersionInfo(
       StringTable(
         u'040904B0',
         [StringStruct(u'CompanyName', u'Tarsier Solutions'),
-        StringStruct(u'FileDescription', u'GUI based ( of ArduinoTalkieSpeech-Py) that convert audio file (WAV) to Talkie (speech synthesis for arduino) compatible data.'),
+        StringStruct(u'FileDescription', u'A simple image to LCD converter compatible to Nokia 3310/5110 LCD'),
         StringStruct(u'FileVersion', u'1.1.8101.99616'),
         StringStruct(u'InternalName', u'cmd'),
         StringStruct(u'LegalCopyright', u'\xa9 Tarsier Solutions. All rights reserved.'),
-        StringStruct(u'OriginalFilename', u'PyAudio2Talkie.exe'),
-        StringStruct(u'ProductName', u'PyAudio-Talkie Synthesis'),
+        StringStruct(u'OriginalFilename', u'Image2LCDConverter.exe'),
+        StringStruct(u'ProductName', u'Image2LCDConverter'),
         StringStruct(u'ProductVersion', u'1.1.8101.99616')])
       ]), 
     VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
